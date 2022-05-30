3b9b8b6500f160a37ba7e20b2b35feecd64f67f6
shinshi <neneloophost@gmail.com>
2022-05-30 11:52:32 +0000

    add feature - home - inquiry - user authentication - article list - article detail

--- article/models.py
+++ article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(CustomUser, verbose_name='name', on_delete=models.PROTECT) 
+    title = models.CharField(verbose_name='title', max_length=100)
+    content = models.TextField(verbose_name='content', blank=True, null=True)
+    photo = models.ImageField(verbose_name='image', blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name='post_time', auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name='update_time', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Article'
+    
+    def __str__(self):
+        return self.title
--- article/views.py
+++ article/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views import generic
+from .forms import InquiryForm
+import logging
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Article
 
 # Create your views here.
+
+logger = logging.getLogger(__name__)
+
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('article:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request,'メッセージを送信しました')
+        logger.info(('Inquiry sent by {}'.format(form.cleaned_data['name'])))
+        return super().form_valid(form)
+        
+class ArticleListView(LoginRequiredMixin, generic.ListView):
+    model = Article
+    template_name = 'article_list.html'
+    paginate_by = 20
+
+    def get_queryset(self):
+        # articles = Article.objects.filter(user=self.request.user).order_by('-created_at')
+        articles = Article.objects.order_by('-created_at')
+        return articles
+
+class ArticleDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    pk_url_kwarg = 'pk'
